f5546f4013f765b3b9a83c9e9a8f18519e13526d
levch <levchenko.d.a1998@gmail.com>
2021-09-23 03:08:52 +0300

    .

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,21 +1,7 @@
+import aiogram_utils
+
 from loader import dp
-from .buttons import CallbackQueryButton, InlineQueryButton, MessageButton
-from .storage import StorageDataFilter
 
 
 def setup():
-    dp.filters_factory.bind(MessageButton, event_handlers=[
-        dp.message_handlers,
-        dp.edited_message_handlers,
-    ])
-    dp.filters_factory.bind(CallbackQueryButton, event_handlers=[
-        dp.callback_query_handlers,
-    ])
-    dp.filters_factory.bind(InlineQueryButton, event_handlers=[
-        dp.inline_query_handlers,
-    ])
-    dp.filters_factory.bind(StorageDataFilter, exclude_event_handlers=[
-        dp.errors_handlers,
-        dp.poll_handlers,
-        dp.poll_answer_handlers,
-    ])
+    aiogram_utils.filters.setup(dp)
